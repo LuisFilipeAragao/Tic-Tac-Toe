6580075b13d829840b06c6ff5596f6dc1687b527
luis.aragao <luisfilipearagaoanjos@gmail.com>
2022-03-30 20:54:32 -0300

    Criando a estrutura - não finalizada

--- main.py
+++ main.py
@@ -4,15 +4,30 @@ import timeit
 # implementar interface gráfica
 # implementar alias para Jogador 1 e Jogador 2
 
-print(f'\nJogo da velha \nX|X|X\t0|0|0\nX|X|X\t0|0|0\nX|X|X\t0|0|0\n')
+print(f'\nTIC\nX|X|X\t0|0|0\nX|X|XTAC0|0|0\nX|X|X\t0|0|0\n\t\t  TOE')
 
-tabuleiro = [['00','01','02'],[11,12,13],[20,21,22]]
-jogo = [[]]
+tabuleiro = [['','',''],['','',''],['','','']]
+jogador1 = ""
+jogador2 = ""
+lance = 0
 
 '''for linha in tabuleiro:
     for coluna in tabuleiro:
         print(coluna)
+
 '''
+'''print(f'Escolha o seu nome, Jogador 1: ')
+jogador1 = input()
+
+print(f'Escolha o seu nome, Jogador 2: ')
+jogador2 = input()
+
+print(f'Preparados? {jogador1} e {jogador2}.\nComeçar!!!')'''
+
+while lance in range(10):
+    print(f'Escolha um número no tabuleiro:\n1 | 2 | 3\n4 | 5 | 6\n7 | 8 | 9')
+
+    lance +=1
 
 # se as linhas ou colunas forem iguais = ponto
 # se a linha e coluna for incrementando +1 ou -1 = ponto
@@ -21,13 +36,3 @@ jogo = [[]]
 # se 0 3 6, 1 4 7, 2 5 8 = vitoria vertical
 # se 0 4 8 , 2 4 6 = vitória diagonal
 
-def ValidaPonto():
-    if jogo[0][0] == jogo[0][1] == jogo[0][2] or jogo[1][0] == jogo[1][1] == jogo[1][2] or jogo[1][0] == jogo[1][1] == jogo[1][2]:
-        # ponto
-    elif jogo[0][0] == jogo[1][0] == jogo[2][0] or jogo[0][1] == jogo[1][1] == jogo[2][1] or jogo[0][2] == jogo[1][2] == jogo[2][2]:
-        #ponto
-    elif jogo[0][0] == jogo[1][1] == jogo[2][2] or jogo[0][3] == jogo[1][1] == jogo[2][1]:
-        #ponto
-    else
-        #nenhum ponto
-        return...
